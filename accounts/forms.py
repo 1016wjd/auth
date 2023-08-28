cf0202ae3ce0ccf0f792bf8dd40d78e842412dd4
KimHyunJung <hday10166@gamil.com>
2023-08-28 15:06:17 +0900

    login

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,9 +1,13 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
 from .models import User
 
 class CustomUserCreationForm(UserCreationForm):
-    class Meta:
+    class Meta: # 우리가 가져온 데이터를 사용하기 위함!
         model = User
         # fields = '__all__'
         fields = ('username', )
+
+
+class CustomAuthenticationForm(AuthenticationForm):
+    pass 
--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,redirect
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, CustomAuthenticationForm
+from django.contrib.auth import login as auth_login
 # Create your views here.
 
 def signup(request):
@@ -16,3 +17,19 @@ def signup(request):
     }
     
     return render(request, 'signup.html', context)
+
+
+def login(request):
+    if request.method == 'POST':
+        form = CustomAuthenticationForm(request, request.POST) # request들어가야함 > 공식문서 확인
+        if form.is_valid():
+            auth_login(request, form.get_user()) #   위에서 로그인 함수 가져와야 함 
+            return redirect('accounts:login')
+    else:
+        form = CustomAuthenticationForm()
+    
+    context = {
+        'form': form,
+    }
+
+    return render(request, 'login.html', context)
